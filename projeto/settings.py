a8c639d8e810e3232ecb3cb8801fc2466102359f
Emanuel Severino <emanuelsmseverino@icloud.com>
2022-11-28 00:19:40 -0300

    Static

--- projeto/settings.py
+++ projeto/settings.py
@@ -69,7 +69,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = Path(BASE_DIR / 'static')
+STATIC_ROOT = Path(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR, 'media')
 
