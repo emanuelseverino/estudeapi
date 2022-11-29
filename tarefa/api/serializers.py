7332fa17b74f7085bd4a6ddb2c17a84334144428
Emanuel Severino <emanuelsmseverino@icloud.com>
2022-11-29 10:34:06 -0300

    Adicionando a App Terafa

--- tarefa/api/serializers.py
+++ tarefa/api/serializers.py
@@ -1,9 +1,15 @@
+from django.db.migrations import serializer
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from tarefa.models import Tarefa
 
 
 class TarefaSerializer(ModelSerializer):
+
+    data = serializers.DateField(required=True)
+    hora = serializers.TimeField(required=True)
+
     class Meta:
         model = Tarefa
         fields = '__all__'
